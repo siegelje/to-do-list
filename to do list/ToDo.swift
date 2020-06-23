2688284dfceb7cdb75a9a8fa7c407df184afc409
Jemma Siegel <jemmasea2003@gmail.com>
2020-06-23 10:12:24 -0400

    toDo files commit one

---
+++ to do list/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  to do list
+//
+//  Created by Jemma Siegel on 6/22/20.
+//  Copyright © 2020 Jemma Siegel. All rights reserved.
+//
+
+import UIKit
+class ToDo {
+    var name = ""
+    var important = false
+}
